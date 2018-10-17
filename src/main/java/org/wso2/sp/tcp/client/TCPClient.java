7f0675f17c1dbea0e5fbd05f054c9c5f461428e9
minudika <minudika001@gmail.com>
2018-10-17 14:48:16 +0530

    Parameterize values

--- src/main/java/org/wso2/sp/tcp/client/TCPClient.java
+++ src/main/java/org/wso2/sp/tcp/client/TCPClient.java
@@ -22,14 +22,17 @@ import org.apache.log4j.Logger;
 import org.wso2.extension.siddhi.io.tcp.transport.TCPNettyClient;
 import org.wso2.extension.siddhi.map.binary.sinkmapper.BinaryEventConverter;
 import org.wso2.siddhi.core.event.Event;
+//import org.wso2.siddhi.core.exception.ConnectionUnavailableException;
 import org.wso2.siddhi.core.exception.ConnectionUnavailableException;
 import org.wso2.siddhi.query.api.definition.Attribute;
+import org.wso2.sp.tcp.client.util.ClientConstants;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -44,27 +47,44 @@ public class TCPClient {
             Attribute.Type.LONG, Attribute.Type.BOOL, Attribute.Type.INT, Attribute.Type.DOUBLE,
             Attribute.Type.DOUBLE, Attribute.Type.FLOAT, Attribute.Type.DOUBLE};
     private static final Logger logger = Logger.getLogger(TCPClient.class);
-    private static final int BATCH_SIZE = 1000;
-    private static final int BATCH_TIME = 1000;
     private static long startTime = 0;
+
+    private static final int DEFAULT_BATCH_SIZE = 1000;
+    private static final int DEFAULT_BATCH_TIME = 1000;
+    private static final String DEFAULT_HOST = "localhost";
+    private static final String DEFAULT_PORT = "9611";
+    private static long interval;
+    private static long duration;
+    private static int batchSize;
 
     /**
      * Main method to start the test client
      */
     public static void main(String[] args) {
         try {
-            String host = args[0];
-            String port = args[1];
-            boolean persistenceEnabled = Boolean.parseBoolean(args[2]);
+            String host = System.getProperty(ClientConstants.HOST, ClientConstants.DEFAULT_HOST);
+            int port = Integer.parseInt(System.getProperty(ClientConstants.PORT, ClientConstants.DEFAULT_PORT));
+            batchSize = Integer.parseInt(System.getProperty(ClientConstants.BATCH_SIZE, ClientConstants
+                    .DEFAULT_BATCH_SIZE));
+            interval = Long.parseLong(System.getProperty(ClientConstants.INTERVAL, ClientConstants
+                    .DEFAULT_INTERVAL));
+            boolean isPersistingEnabled = Boolean.parseBoolean(System.getProperty(ClientConstants.PERSISTENCE_ENABLED,
+                    "false"));
+            duration = Long.parseLong(System.getProperty(ClientConstants.DURATION, ClientConstants
+                    .DEFAULT_DURATION));
 
+            logger.info("**********************");
             logger.info("Host : " + host);
             logger.info("Port : " + port);
-            logger.info("persistenceEnabled : " + persistenceEnabled);
-
+            logger.info("Batch Size : " + batchSize);
+            logger.info("Time between batches : " + interval);
+            logger.info("Total duration : " + duration);
+            logger.info("Is a persisting scenario : " + isPersistingEnabled);
+            logger.info("**********************");
             TCPNettyClient tcpNettyClient = new TCPNettyClient();
-            tcpNettyClient.connect(host, Integer.parseInt(port));
+            tcpNettyClient.connect(host, port);
             logger.info("TCP client connected");
-            if (!persistenceEnabled) {
+            if (!isPersistingEnabled) {
                 publishPassthroughMessages(tcpNettyClient);
             } else {
                 publishPersistingMessages(tcpNettyClient);
@@ -82,24 +102,25 @@ public class TCPClient {
     private static void publishPassthroughMessages(TCPNettyClient tcpNettyClient) throws IOException {
         Random rand = new Random();
         long i = 0;
-        for (; i < EVENT_COUNT; i += BATCH_SIZE) {
-            startTime = System.currentTimeMillis();
-            ArrayList<Event> arrayList = new ArrayList<Event>(BATCH_SIZE);
-            for (int j = 0; j < BATCH_SIZE; j++) {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < duration) {
+            long batchStartTime = System.currentTimeMillis();
+            List<Event> arrayList = new ArrayList<Event>(batchSize);
+            for (int j = 0; j < batchSize; j++) {
                 Event event = new Event();
                 event.setData(new Object[]{System.currentTimeMillis(), rand.nextFloat()});
 
                 long time = System.currentTimeMillis();
-                if (time - startTime <= BATCH_TIME) {
+                if (time - batchStartTime <= interval) {
                     arrayList.add(event);
                 }
             }
             tcpNettyClient.send(STREAM_NAME, BinaryEventConverter.convertToBinaryMessage(
                     arrayList.toArray(new Event[0]), PASSTHROUGH_TYPES).array());
             long currentTime = System.currentTimeMillis();
-            if (currentTime - startTime <= BATCH_TIME) {
+            if (currentTime - batchStartTime <= interval) {
                 try {
-                    Thread.sleep(BATCH_TIME - (currentTime - startTime));
+                    Thread.sleep(interval - (currentTime - batchStartTime));
                 } catch (InterruptedException ex) {
                     logger.error(ex);
                 }
@@ -115,25 +136,26 @@ public class TCPClient {
     private static void publishPersistingMessages(TCPNettyClient tcpNettyClient) throws IOException {
         Random rand = new Random();
         long i = 0;
-        for (; i < EVENT_COUNT; i += BATCH_SIZE) {
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < duration) {
             startTime = System.currentTimeMillis();
-            ArrayList<Event> arrayList = new ArrayList<Event>(BATCH_SIZE);
-            for (int j = 0; j < BATCH_SIZE; j++) {
+            ArrayList<Event> arrayList = new ArrayList<Event>(batchSize);
+            for (int j = 0; j < batchSize; j++) {
                 Event event = new Event();
                 event.setData(new Object[]{System.currentTimeMillis(), rand.nextBoolean(), rand.nextInt(), rand
                         .nextDouble(), rand.nextDouble(), rand.nextFloat(), rand.nextDouble()});
 
                 long time = System.currentTimeMillis();
-                if (time - startTime <= BATCH_TIME) {
+                if (time - startTime <= interval) {
                     arrayList.add(event);
                 }
             }
             tcpNettyClient.send(STREAM_NAME, BinaryEventConverter.convertToBinaryMessage(
                     arrayList.toArray(new Event[0]), PERSISTING_TYPES).array());
             long currentTime = System.currentTimeMillis();
-            if (currentTime - startTime <= BATCH_TIME) {
+            if (currentTime - startTime <= interval) {
                 try {
-                    Thread.sleep(BATCH_TIME - (currentTime - startTime));
+                    Thread.sleep(interval - (currentTime - startTime));
                 } catch (InterruptedException ex) {
                     logger.error(ex);
                 }
---
+++ src/main/java/org/wso2/sp/tcp/client/util/ClientConstants.java
@@ -0,0 +1,32 @@
+package org.wso2.sp.tcp.client.util;
+
+/**
+ * Constants
+ */
+public class ClientConstants {
+    public static final String HOST = "host";
+    public static final String PORT = "port";
+    public static final String BATCH_SIZE = "batch.size";
+    public static final String INTERVAL = "interval";
+    public static final String DURATION = "duration";
+    public static final String THREAD_COUNT = "thread.count";
+    public static final String SCENARIO = "scenario";
+    public static final String PERSISTENCE_ENABLED = "persistence.enabled";
+
+    public static final String DEFAULT_HOST = "localhost";
+    public static final String DEFAULT_PORT = "9892";
+    public static final String DEFAULT_BATCH_SIZE = "1000";
+    public static final String DEFAULT_THREAD_COUNT = "1";
+    public static final String DEFAULT_INTERVAL =  "1000";
+    public static final String DEFAULT_DURATION =  "2400000000";
+
+    public static final String FILTER = "filter";
+    public static final String PARTITIONS = "partitions";
+    public static final String PATTERNS = "patterns";
+    public static final String PERSISTENCE_MYSQL = "persistence.mysql";
+    public static final String PERSISTENCE_MSSQL = "persistence.mssql";
+    public static final String PERSISTENCE_ORACLE = "persistence.oracle";
+    public static final String PASSTHROUGH = "passthrough";
+    public static final String WINDOW_LARGE = "large.window";
+    public static final String WINDOW_SMALL = "small.window";
+}
